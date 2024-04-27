802e4f8b5dcd9cdfc815d4b4732f2592f24fac17
Sonki <sonialasen.slg@gmail.com>
2024-04-27 20:58:55 +0200

    finalizado

--- app/src/main/java/com/example/datastore/MainActivity.kt
+++ app/src/main/java/com/example/datastore/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -28,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.datastore.core.DataStore
 import com.example.datastore.ui.theme.DataStoreTheme
 import kotlinx.coroutines.launch
 
@@ -36,13 +33,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DataStoreTheme {
+            val darkModeStore = StoreDarkMode(this)//this es el contexto de la MainActivity
+            val darkMode = darkModeStore.getDarkMode.collectAsState(initial = false)
+            DataStoreTheme ( darkTheme = darkMode.value){
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    Greeting(darkModeStore, darkMode.value)
                 }
             }
         }
@@ -50,7 +49,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
+fun Greeting(darkModeStore : StoreDarkMode, darkMode : Boolean) {
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val dataStore = StoreUserEmail(context)
@@ -74,6 +73,12 @@ fun Greeting() {
         }
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = userEmail.value)
-
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {scope.launch {
+            if (darkMode) darkModeStore.saveDarkMode(false)
+            else darkModeStore.saveDarkMode(true)
+        } }) {
+            Text(text = "Dark Mode")
+        }
     }
 }
--- app/src/main/java/com/example/datastore/StoreDarkMode.kt
+++ app/src/main/java/com/example/datastore/StoreDarkMode.kt
@@ -1,4 +1,29 @@
 package com.example.datastore
 
-class StoreDarkMode {
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class StoreDarkMode (private val context: Context) {
+
+    companion object{
+        private val Context.dataStore : DataStore<Preferences> by preferencesDataStore("DarkMode")//nombre del proyecto en DataStore Preferences
+        val DARK_MODE = booleanPreferencesKey("dark_mode")//nombre de la clave, del campo que vamos a almacenar
+    }
+
+    val getDarkMode : Flow<Boolean> =context.dataStore.data //así decimos que es una variable flow sin ser en el ViewModel
+        .map {
+            it[DARK_MODE] ?: false //se puede quitar el operador elvis y que sea de tipo String?
+        }
+
+    suspend fun saveDarkMode(dMode : Boolean){//usamos parámetros porque luego los usamos desde la vista, lo que escribamos en el TextField se guarda en este DataStore
+        context.dataStore.edit {
+            it[DARK_MODE] = dMode
+        }
+    }
 }
